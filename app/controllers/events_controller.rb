2e0aef8a8ac90daed2e33de2f9030d03d262bb51
David Ochoa <ochoaqdavid@gmail.com>
2014-07-29 23:27:22 +0300

    Add basic implementation of events#index results

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,14 @@
 class EventsController < ApplicationController
   def index
+    @events = Event.search_by(@graph, permitted_params)
   end
 
   def show
+  end
+
+  private
+
+  def permitted_params
+    params.permit(:q)
   end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,17 @@
 class Event
+  class << self
+    def search_by(fb_graph, params)
+      results = fb_graph.search(params[:q], type: 'event')
+      results = filter_results(results)
+    end
+
+    private
+
+    def filter_results(results)
+      results.select! do |x|
+        !x['start_time'].blank? && x['start_time'] > Time.now
+      end
+      results.sort! { |a,b| a['start_time'] <=> b['start_time'] }
+    end
+  end
 end
