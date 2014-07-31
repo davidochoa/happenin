615f117b302c745f71ea8d333f3c8e1637c760a7
David Ochoa <ochoaqdavid@gmail.com>
2014-07-31 22:30:07 +0300

    Changes to events#index styling and information

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,9 @@ class ApplicationController < ActionController::Base
     init_facebook_user unless session[:fb_access_token]
     @graph = Koala::Facebook::API.new(session[:fb_access_token])
     @graph.debug_token(session[:fb_access_token])
-  rescue Koala::Facebook::AuthenticationError, Koala::Facebook::ClientError
+  rescue Koala::Facebook::AuthenticationError,
+      Koala::Facebook::ClientError => e
+    reset_facebook_access_token(e)
     render 'home/facebook_login'
   end
 
@@ -21,5 +23,12 @@ class ApplicationController < ActionController::Base
     oauth = Koala::Facebook::OAuth.new(app_id, app_secret)
     user_info = oauth.get_user_info_from_cookies(cookies)
     session[:fb_access_token] = user_info['access_token'] if user_info
+  end
+
+  def reset_facebook_access_token(e)
+    return unless e && e.fb_error_type == 'OAuthException' &&
+      e.fb_error_code == 190 && e.fb_error_subcode == 463
+    session[:fb_access_token] = nil
+    init_facebook_graph
   end
 end
