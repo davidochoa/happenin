ffab1cae8a84f48cd830e581ebddf858f86afbc1
David Ochoa <ochoaqdavid@gmail.com>
2014-08-07 22:04:32 +0300

    Change the structure of event model and parameter retrieval

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,8 @@ class Event
   SOURCE = 01
 
   attr_reader :id, :name, :description, :location, :start_time, :end_time,
-              :timezone, :privacy, :venue, :owner, :ticket_uri
+              :timezone, :privacy, :venue_name, :owner_name, :owner_url,
+              :ticket_url
 
   class << self
     def search_by(fb_graph:, params:)
@@ -27,24 +28,81 @@ class Event
   end
 
   def initialize(fb_graph: nil, params:)
-    params = event_information(fb_graph: fb_graph, id: params[:id]) if fb_graph
-    event_parameterize(params: params)
+    @raw_object =
+      fb_graph.blank? ? params : src_object(fb_graph: fb_graph, id: params['id'])
   end
 
   def source_id
     SOURCE
   end
 
+  def id
+    object_param(param: :id)
+  end
+
+  def name
+    object_param(param: :name)
+  end
+
+  def description
+    object_param(param: :description)
+  end
+
+  def location
+    object_param(param: :location)
+  end
+
+  def start_time
+    object_param(param: :start_time)
+  end
+
+  def end_time
+    object_param(param: :end_time)
+  end
+
+  def timezone
+    object_param(param: :timezone)
+  end
+
+  def privacy
+    object_param(param: :privacy)
+  end
+
+  def venue
+    return if @raw_object.blank? || @raw_object['venue'].blank? ||
+      @raw_object['venue']['latitude'].blank? ||
+      @raw_object['venue']['longitude'].blank?
+    {
+      latitude: @raw_object['venue']['latitude'],
+      longitude: @raw_object['venue']['longitude']
+    }
+  end
+
+  def owner_name
+    return if @raw_object.blank? || @raw_object['owner'].blank? ||
+      @raw_object['owner']['name'].blank?
+    @raw_object['owner']['name']
+  end
+
+  def owner_url
+    return if @raw_object.blank? || @raw_object['owner'].blank? ||
+      @raw_object['owner']['id'].blank?
+    "//www.facebook.com/#{@raw_object['owner']['id']}"
+  end
+
+  def ticket_url
+    object_param(param: :ticket_uri)
+  end
+
   private
 
-  def event_information(fb_graph:, id:)
-    return unless fb_graph
+  def src_object(fb_graph:, id:)
     fb_graph.get_object(id)
   end
 
-  def event_parameterize(params: params)
-    params.each do |key, value|
-      instance_variable_set("@#{key}", value)
-    end
+  def object_param(param:)
+    return if @raw_object.blank? || param.blank? ||
+      @raw_object[param.to_s].blank?
+    @raw_object[param.to_s]
   end
 end
